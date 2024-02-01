2dd38f6db8c1259ddd4a01ac7c08bcfc018c5a1b
Pablo Brizzi <139920705+paulusbrizzi@users.noreply.github.com>
2024-02-01 19:15:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 from fastapi import FastAPI
 import os
 import pandas as pd
-import pyarrow
-import fastparquet
+
 
 app = FastAPI()
 
-games = pd.read_parquet("nuevo/games_API.parquet")
-reviews = pd.read_parquet("nuevo/reviews_games_API.parquet")
-items = pd.read_parquet("nuevo/items_games_API.parquet")
-
-
-# 1
+##### 1
+# Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora.
 @app.get("/developer")
 def developer(desarrollador : str):
-# filtramos por desarrollador
-    filtro = games[games["developer"] == desarrollador]
-# no quedamos con nuestras columnas de interés
-    reviews1 = reviews[["item_id", "year_release"]]
-# hacemos un merge de tablas
-    join = pd.merge(filtro, reviews1, on="item_id", how="inner")
+# importamos la tabla de la función
+    developer = pd.read_csv("data_api/developer.csv")
+# filtramos por developer
+    developer = developer[developer["developer"] == desarrollador]
 # agrupamos por año, contamos los items y calculamos el porcentaje free
-    agrupado = join.groupby('year_release').agg({
+    agrupado = developer.groupby('year_release').agg({
     'item_id': 'count',
     'price': lambda x: (x == 0.00).mean() * 100
     }).reset_index()
@@ -35,17 +28,16 @@ def developer(desarrollador : str):
     return diccionario_resultado
 
 
-# 2
+##### 2
+# Cantidad de dinero gastado por el usuario, el porcentaje de recomendación y cantidad de items.
 @app.get("/userdata")
 def userdata(user_id : str):
-# filtramos por usuario
-    filtro = reviews[reviews["user_id"] == user_id]
-# nos quedamos con nuestras columnas de interés
-    filtro_join = games[["item_id", "price"]]
-# creamos una nueva tabla
-    merge = pd.merge(filtro, filtro_join, on="item_id", how="inner") 
+# importamos la tabla de la función
+    userdata = pd.read_csv("data_api/userdata.csv")
+#filtramos por usuario
+    userdata = userdata[userdata["user_id"] == user_id]
 # nos quedamos con las columnas de interés
-    seleccion = merge[["user_id", "item_id", "recommend", "price"]]  
+    seleccion = userdata[["user_id", "item_id", "recommend", "price"]]  
 # agrupamos por usuario, contamos los items, calculamos el porcentaje y sumamos precios
     userdata = seleccion.groupby("user_id").agg({
     'item_id': 'count',
@@ -56,76 +48,72 @@ def userdata(user_id : str):
     userdata.columns = ['Usuario', 'Items', 'Porcentaje de recomendación', "Dinero gastado"]
     userdata = userdata[['Usuario', 'Dinero gastado', 'Porcentaje de recomendación', 'Items']]
 # transformamos el resultado
-    diccionario_resultado = userdata.to_dict(orient='records')[0]
+    diccionario_resultado = userdata.to_dict(orient='records')
 
     return diccionario_resultado
 
 
-# 3
+
+##### 3
+# Usuario que acumula más horas jugadas para el género dado y lista de la acumulación de horas jugadas por año de lanzamiento.
 @app.get("/user_for_genre")
 def user_for_genre(genero : str):
+# importamos la primera tabla
+    u1 = pd.read_csv("data_api/user_for_genre1.csv")
 # filtramos por género
-    filtro = items[items["game_genre"] == genero]
-# buscamos el usuario con mayor playtime_forever
-    mascara = filtro["playtime_forever"].max()
-    user = filtro["user_id"][mascara]
-# agrupamos por total de horas por año
-    horas_jugadas = filtro.groupby('year_release').agg({
-    'playtime_forever': 'sum'
-    }).reset_index()
-# corregimos el orden a descendente
-    horas_jugadas = horas_jugadas.sort_values(by='year_release', ascending=False)
+    u1 = u1["user_id"][u1["game_genre"] == genero]
+    u1 = u1.values[0]
+# importamos la segunda tabla
+    u2 = pd.read_csv("data_api/user_for_genre2.csv")
+# nos quedamos con las columnas de interés según género
+    u2 = u2[["year_release", "playtime_forever"]][u2["game_genre"] == genero]
+# ordenamos según año
+    horas_jugadas = u2.sort_values(by='year_release', ascending=False)
 # cambiamos el nombre de las columnas
     horas_jugadas.columns = ["Año", "Horas"]
 # convertimos el df en lista
     lista_resultado = horas_jugadas.to_dict(orient='records')
 # formateamos la salida
-    out = {"Usuario con más horas jugadas para Género X" : user, "Horas jugadas": lista_resultado}
+    out = {f"Usuario con más horas jugadas para Género {genero}" : u1, "Horas jugadas": lista_resultado}
 
     return out
 
 
-# 4
-@app.get("/developer_reviews_analysis")
+##### 4
+# Top 3 de desarrolladores con juegos más recomendados por usuarios para el año dado.
+@app.get("/best_developer_year")
 def best_developer_year(año : int):
-# trabajamos con nuestras columnas de interés y formamos una nueva tabla
-    games = games[["item_id", "developer"]]
-    reviews = reviews[["item_id","recommend", "year_posted"]]
-    merge = pd.merge(games, reviews, on="item_id", how="inner")
-# filtramos de acuerdo al parámetro
-    filtro = merge[merge["year_posted"] == año]
-# agrupamos según suma de recommend por developer
-    agrupado = filtro.groupby('developer').agg({
-    'recommend': 'sum'
-    }).reset_index()
-# filtramos por ascendente y pedimos solo los 3 primeros
-    agrupado = agrupado.sort_values("recommend", ascending=False).head(3)
-    agrupado = agrupado.reset_index()
-# nos quedamos con la columna de interés
-    agrupado = agrupado["developer"]
-# convertimos a dataframe para asegurar el formateo
-    agrupado = pd.DataFrame(agrupado)
-# formateamos el resultado
-    valores = agrupado['developer'].tolist()
-    lista_resultado = [{"Puesto {}: ".format(i + 1): valor} for i, valor in enumerate(valores)]
+# importamos la tabla a usar
+    bdy = pd.read_csv("data_api/best_developer_year.csv")
+# nos quedamos con las columnas de utilidad
+    bdy = bdy[["Año", "Desarrollador"]]
+# filtramos desarrolladores top por año
+    filtro = bdy["Desarrollador"][bdy["Año"] == año]
+# convertimos cada valor en una variable independiente
+    X = filtro.iloc[0]
+    Y = filtro.iloc[1]
+    Z = filtro.iloc[2]
+# formateamos la salida
+    lista_resultado = [{"Puesto 1" : f"{X}"}, {"Puesto 2" : f"{Y}"},{"Puesto 3" : f"{Z}"}]
 
     return lista_resultado
 
 
-# 5
+##### 5
+# diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registro
+# de reseñas como valor positivo o negativo.
 @app.get("/developer_reviews_analysis")
 def developer_reviews_analysis(desarrollador : str):
-# trabajamos con nuestras columnas de interés y formamos una nueva tabla
-    games = games[["item_id", "developer"]]
-    reviews = reviews[["item_id", "analisis_sentimiento"]]
-    merge = pd.merge(games, reviews, on="item_id", how="inner")
+# importamos la tabla a usar
+    dra = pd.read_csv("data_api/developer_reviews_analysis.csv")
+# nos quedamos con las columnas de utilidad
+    dra = dra[["developer", "Negative", "Positive"]]
 #filtramos de acuerdo al parámetro
-    filtro = merge[merge["developer"] == desarrollador]
-# contamos cada valor del analisis_sentimiento
-    conteo_sentimientos = filtro['analisis_sentimiento'].value_counts()
-# creamos una lista
-    lista_resultado = [f'Negative = {conteo_sentimientos.get(0, 0)}', f'Positive = {conteo_sentimientos.get(2, 0)}']
-# convertimos a dict
-    out = {desarrollador: lista_resultado}
+    filtro = dra[["Negative", "Positive"]][dra["developer"] == desarrollador]
+# creamos las variables para el formateo del return
+    neg = filtro["Negative"].iloc[0]
+    pos = filtro["Positive"].iloc[0]
+# creamos el diccionario de salida
+    out = {f'{desarrollador}' : [f"Negative = {neg}", f"Positive = {pos}"]}
 
     return out
